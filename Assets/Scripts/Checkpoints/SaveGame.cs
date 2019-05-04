d1bc09f3bd7e21f9ee62304d32375fb2f69c7eaa
elfefe <f.bou-reiff@orange.fr>
2019-05-04 15:19:22 +0200

    Création de la prefab checkpoint. Cette prefab permet de sauvegarder la position du joueur, une fois utilisé elle est désactivée.

---
+++ Assets/Scripts/Checkpoints/SaveGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGame : MonoBehaviour
+{
+    public const string X_POSITION = "x_position";
+    public const string Y_POSITION = "y_position";
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerPrefs.SetFloat(X_POSITION, transform.position.x);
+            PlayerPrefs.SetFloat(Y_POSITION, transform.position.y);
+            PlayerPrefs.Save();
+        }
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
