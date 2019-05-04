0671895137c3c2f5f87f4d3deeb6a34b833e33cd
elfefe <f.bou-reiff@orange.fr>
2019-05-04 14:26:02 +0200

    Ajout des dossiers Prefab/Mecanics et de la prefab Spawner.

--- Assets/Scripts/SpawnMobs.cs
+++ Assets/Scripts/SpawnMobs.cs
@@ -7,6 +7,8 @@ public class SpawnMobs : MonoBehaviour
 
     public GameObject prefab;
 
+    private bool cameraEnter;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -16,6 +18,10 @@ public class SpawnMobs : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (cameraEnter)
+        {
+            GameObject mob = (GameObject)Instantiate(prefab);
+            mob.transform.position = this.transform.position;
+        }
     }
 }
