7cfe7f277908502dfb67665d8c19efc798edb3fd
elfefe <f.bou-reiff@orange.fr>
2019-05-05 01:48:25 +0200

    Panel Rotation.

--- Assets/Scripts/Steps/HintPanelTrigger.cs
+++ Assets/Scripts/Steps/HintPanelTrigger.cs
@@ -6,32 +6,36 @@ public class HintPanelTrigger : MonoBehaviour
 {
     public GameObject hintPanelPrefab, hintPanelSpawner,HintPanelTarget;
     public float speed;
-    private bool hasAppeared = false;
 
     private GameObject hintPanel;
     private Vector3 characterPosition;
 
     private void Update()
     {
-        if (hasAppeared)
+        if (hintPanel != null)
         {
-            hintPanel.transform.position = Vector3.MoveTowards(hintPanel.transform.position, characterPosition, speed * Time.deltaTime);
-            if (hintPanel.transform.position == characterPosition)
+            if (hintPanel.transform.position != characterPosition)
             {
-                hasAppeared = false;
+                hintPanel.transform.position = Vector3.MoveTowards(hintPanel.transform.position, characterPosition, speed * Time.deltaTime);
+                hintPanel.transform.rotation = Quaternion.RotateTowards(hintPanel.transform.rotation, HintPanelTarget.transform.rotation, speed * Time.deltaTime * 6f);
             }
         }
-        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            hintPanel = Instantiate(hintPanelPrefab, hintPanelSpawner.transform.position, Quaternion.identity);
             characterPosition = HintPanelTarget.transform.position;
+            Vector2 randomPosition = new Vector2(Mathf.PerlinNoise(0,1) * 10, hintPanelSpawner.transform.position.y);
+
+            hintPanel = Instantiate(hintPanelPrefab, randomPosition, Quaternion.LookRotation(new Vector3(0, 0, HintPanelTarget.transform.position.z)));
+
+            float AngleRad = Mathf.Atan2(characterPosition.y - hintPanel.transform.position.y, characterPosition.x - hintPanel.transform.position.x);
+            float AngleDeg = (180 / Mathf.PI) * AngleRad;
+            hintPanel.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+
             GetComponent<BoxCollider2D>().enabled = false;
-            hasAppeared = true;
         }
     }
 }
