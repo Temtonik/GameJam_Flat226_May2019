33676c1a15ab44854c14806b4b8e9d638985a73e
elfefe <f.bou-reiff@orange.fr>
2019-05-04 23:14:34 +0200

    Ajout des panneaux d'indications.

--- Assets/Scripts/Steps/HintPanelTrigger.cs
+++ Assets/Scripts/Steps/HintPanelTrigger.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PanelTrigger : MonoBehaviour
+public class HintPanelTrigger : MonoBehaviour
 {
-    public GameObject panel;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
