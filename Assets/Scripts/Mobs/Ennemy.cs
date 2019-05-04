006afae499924526b9629e75708f690638bb1ed8
elfefe <f.bou-reiff@orange.fr>
2019-05-04 14:55:19 +0200

    Ajout de la possibilité de tuer l'ennemy.

--- Assets/Scripts/Mobs/Ennemy.cs
+++ Assets/Scripts/Mobs/Ennemy.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Ennemy : MonoBehaviour
 {
+    public GameObject poupeeVaudouPrefab;
+
+    private bool isKilled = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
+        } else if (collision.gameObject.CompareTag("Louche"))
+        {
+            isKilled = true;
+            Destroy(gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        GameObject poupeeVaudou = Instantiate(poupeeVaudouPrefab, gameObject.transform.position, Quaternion.identity);
     }
 }
