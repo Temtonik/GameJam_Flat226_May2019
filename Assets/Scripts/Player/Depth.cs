99cf2e5b1219086d6a235668c879ef9d86fa2e4b
elfefe <f.bou-reiff@orange.fr>
2019-05-05 12:12:09 +0200

    Simplification de la mise en place de l'effet parralax.

---
+++ Assets/Scripts/Player/Depth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Depth : MonoBehaviour
+{
+    public int orderInLayer;
+    public int zPosition;
+
+    private void Awake()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            child.GetComponent<SpriteRenderer>().sortingOrder = orderInLayer;
+            child.transform.position = new Vector3(child.transform.position.x, child.transform.position.y, -zPosition);
+        }
+    }
+}
