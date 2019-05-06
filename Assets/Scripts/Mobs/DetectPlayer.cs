4558a491ad56c7b536c29ed9397b7b339036fee2
elfefe <f.bou-reiff@orange.fr>
2019-05-06 16:34:20 +0200

    Configuration de l'araigne, ajout du fil extensible.

---
+++ Assets/Scripts/Mobs/DetectPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+    public GameObject spider;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        spider.GetComponent<SpiderManager>().IsGoingDown = true;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
